721080b08f46c2f3d047d7a332c2a25006af8217
Arare1268 <126842416+Arare1268@users.noreply.github.com>
2023-11-21 11:19:58 +0800

    const

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -6,7 +6,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       initialRoute: AppRoutes.home,
       onGenerateRoute: AppRoutes.generateRoute,
     );
