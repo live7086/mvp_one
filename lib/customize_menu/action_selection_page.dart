be2887965ad9425c90547aec1b97c59e0a50b3bc
Lyndon <luzzyyo@gmail.com>
2024-05-01 09:18:08 +0800

    allMainPages 新增 旋轉木馬 菜單選擇頁面修改

--- lib/customize_menu/action_selection_page.dart
+++ lib/customize_menu/action_selection_page.dart
@@ -19,6 +19,13 @@ class _ActionSelectionPageState extends State<ActionSelectionPage> {
   List<Meal> selectedMealsForCustomMenu = []; // 為自定義菜單選擇的餐點列表
   String customMenuTitle = ''; // 自定義菜單標題
   String customMenuDescription = ''; // 自定義菜單描述
+  void _reorderSelectedMeal(int oldIndex, int newIndex) {
+    if (oldIndex < newIndex) {
+      newIndex -= 1;
+    }
+    final Meal selectedMeal = selectedMealsForCustomMenu.removeAt(oldIndex);
+    selectedMealsForCustomMenu.insert(newIndex, selectedMeal);
+  }
 
   @override
   void initState() {
@@ -111,28 +118,64 @@ class _ActionSelectionPageState extends State<ActionSelectionPage> {
       appBar: AppBar(
         title: const Text('選擇動作'),
       ),
-      body: ListView.builder(
-        itemCount: availableMeals.length,
-        itemBuilder: (context, index) {
-          final meal = availableMeals[index];
-          return _MealItem(
-            meal: meal,
-            isSelected: selectedMealsForCustomMenu.contains(meal),
-            onSelectMeal: (selectedMeal) {
-              setState(() {
-                if (selectedMealsForCustomMenu.contains(selectedMeal)) {
-                  selectedMealsForCustomMenu.remove(selectedMeal);
-                } else {
-                  selectedMealsForCustomMenu.add(selectedMeal);
-                }
-              });
-            },
-          );
-        },
+      body: Column(
+        children: [
+          Expanded(
+            child: GridView.builder(
+              padding: const EdgeInsets.all(10),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.8,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+              ),
+              itemCount: availableMeals.length,
+              itemBuilder: (context, index) {
+                final meal = availableMeals[index];
+                return _MealItem(
+                  meal: meal,
+                  isSelected: selectedMealsForCustomMenu.contains(meal),
+                  onSelectMeal: (selectedMeal) {
+                    setState(() {
+                      if (selectedMealsForCustomMenu.contains(selectedMeal)) {
+                        selectedMealsForCustomMenu.remove(selectedMeal);
+                      } else {
+                        selectedMealsForCustomMenu.add(selectedMeal);
+                      }
+                    });
+                  },
+                );
+              },
+            ),
+          ),
+          Container(
+            height: 100,
+            color: Colors.grey[200],
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: selectedMealsForCustomMenu.length,
+              itemBuilder: (context, index) {
+                final selectedMeal = selectedMealsForCustomMenu[index];
+                return _SelectedMealItem(
+                  meal: selectedMeal,
+                  onRemove: () {
+                    setState(() {
+                      selectedMealsForCustomMenu.remove(selectedMeal);
+                    });
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _completeSelection,
-        child: const Icon(Icons.check),
+        backgroundColor: Colors.blue,
+        child: const Icon(
+          Icons.check,
+          color: Colors.white,
+        ),
       ),
     );
   }
@@ -161,47 +204,98 @@ class _MealItem extends StatelessWidget {
           borderRadius: BorderRadius.circular(15),
           side: BorderSide(
             color: isSelected ? Colors.blue : Colors.transparent,
-            width: 2,
+            width: 3,
           ),
         ),
-        elevation: 4,
-        margin: const EdgeInsets.all(10),
-        child: Column(
+        elevation: isSelected ? 12 : 4,
+        margin: const EdgeInsets.all(0),
+        child: Stack(
           children: [
-            Stack(
-              children: [
-                ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(15),
-                    topRight: Radius.circular(15),
-                  ),
-                  child: Image(
-                    image: meal.imageUrl,
-                    height: 250,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Positioned(
-                  bottom: 20,
-                  right: 10,
-                  child: Container(
-                    width: 300,
-                    color: Colors.black54,
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-                    child: Text(
-                      meal.title,
-                      style: const TextStyle(fontSize: 26, color: Colors.white),
-                      softWrap: true,
-                      overflow: TextOverflow.fade,
-                    ),
-                  ),
-                ),
-              ],
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Image(
+                image: meal.imageUrl,
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
+              ),
             ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.6),
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(15),
+                    bottomRight: Radius.circular(15),
+                  ),
+                ),
+                child: Text(
+                  meal.title,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ),
+            if (isSelected)
+              Positioned(
+                top: 10,
+                right: 10,
+                child: Icon(
+                  Icons.check_circle,
+                  color: Colors.blue,
+                  size: 30,
+                ),
+              ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _SelectedMealItem extends StatelessWidget {
+  final Meal meal;
+  final VoidCallback onRemove;
+
+  const _SelectedMealItem({
+    Key? key,
+    required this.meal,
+    required this.onRemove,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(5),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image(
+              image: meal.imageUrl,
+              width: 80,
+              height: 60,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(height: 5),
+          GestureDetector(
+            onTap: onRemove,
+            child: const Icon(
+              Icons.remove_circle,
+              color: Colors.red,
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,22 +1,23 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:mvp_one/data/dummy_data.dart';
 import 'package:mvp_one/models/category.dart';
-// ignore: unused_import
 import 'package:mvp_one/models/meal.dart';
 import 'package:mvp_one/widgets/category_grid_item.dart';
 import 'package:mvp_one/screens/move.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen(
-      {super.key,
-      required this.onToggleFavorite,
-      required this.availableMeals});
+  const CategoriesScreen({
+    super.key,
+    required this.onToggleFavorite,
+    required this.availableMeals,
+  });
 
   final void Function(Meal meal) onToggleFavorite;
   final List<Meal> availableMeals;
 
   void _selectCategory(BuildContext context, Category category) {
-    final filiteredMeals = availableMeals
+    final filteredMeals = availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
@@ -24,7 +25,7 @@ class CategoriesScreen extends StatelessWidget {
       MaterialPageRoute(
         builder: (ctx) => MealsScreen(
           title: category.title,
-          meals: filiteredMeals,
+          meals: filteredMeals,
           onToggleFavorite: onToggleFavorite,
         ),
       ),
@@ -33,23 +34,94 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(24),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 1,
-        childAspectRatio: 2.2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    final List<String> _carouselImages = [
+      'assets/car-item/car-item-1.jpg',
+      'assets/car-item/car-item-2.jpg',
+      'assets/car-item/car-item-3.jpg',
+      'assets/car-item/car-item-4.jpg',
+    ];
+
+    final List<String> _carouselText = [
+      '快速伸展',
+      '肌肉放鬆',
+      '新手入門',
+      '30天挑戰',
+    ];
+
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          CarouselSlider(
+            items: _carouselImages.map((imagePath) {
+              final index = _carouselImages.indexOf(imagePath);
+              final text = _carouselText[index];
+              return Container(
+                margin: EdgeInsets.all(6.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Stack(
+                    children: [
+                      Image.asset(
+                        imagePath,
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                      ),
+                      Positioned.fill(
+                        child: Container(
+                          color: Colors.black.withOpacity(0.5),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 16.0,
+                        left: 16.0,
+                        right: 16.0,
+                        child: Text(
+                          text,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }).toList(),
+            options: CarouselOptions(
+              height: MediaQuery.of(context).size.height * 0.2,
+              autoPlay: true,
+              enlargeCenterPage: true,
+              aspectRatio: 16 / 9,
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enableInfiniteScroll: true,
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              viewportFraction: 0.8,
+            ),
+          ),
+          GridView(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            padding: const EdgeInsets.all(24),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              childAspectRatio: 2.2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20,
+            ),
+            children: [
+              for (final category in availableCategories)
+                CategoryGridItem(
+                  category: category,
+                  onSelectCategory: () {
+                    _selectCategory(context, category);
+                  },
+                ),
+            ],
+          ),
+        ],
       ),
-      children: [
-        for (final category in availableCategories)
-          CategoryGridItem(
-            category: category,
-            onSelectCategory: () {
-              _selectCategory(context, category);
-            },
-          )
-      ],
     );
   }
 }
