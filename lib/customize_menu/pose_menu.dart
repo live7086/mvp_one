af46268143781e4dac60d615bf9dfd2058e0ac9d
Lyndon <luzzyyo@gmail.com>
2024-05-05 20:31:34 +0800

    posepainter fixed

--- lib/customize_menu/pose_menu.dart
+++ lib/customize_menu/pose_menu.dart
@@ -1102,69 +1102,6 @@ class MenuCameraScreenState extends State<MenuCameraScreen>
   }
 }
 
-// 繪製身體骨架
-class PosePainter extends CustomPainter {
-  final List<Pose> poses;
-  final bool isFrontCamera;
-
-  PosePainter(this.poses, this.isFrontCamera);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = Colors.red
-      ..strokeWidth = 5;
-
-    for (var pose in poses) {
-      final landmarks = pose.landmarks.values.toList();
-
-      // 定義身體部位之間的連接關係
-      final connections = [
-        [PoseLandmarkType.leftShoulder, PoseLandmarkType.rightShoulder],
-        [PoseLandmarkType.leftShoulder, PoseLandmarkType.leftElbow],
-        [PoseLandmarkType.leftElbow, PoseLandmarkType.leftWrist],
-        [PoseLandmarkType.rightShoulder, PoseLandmarkType.rightElbow],
-        [PoseLandmarkType.rightElbow, PoseLandmarkType.rightWrist],
-        [PoseLandmarkType.leftShoulder, PoseLandmarkType.leftHip],
-        [PoseLandmarkType.rightShoulder, PoseLandmarkType.rightHip],
-        [PoseLandmarkType.leftHip, PoseLandmarkType.rightHip],
-        [PoseLandmarkType.leftHip, PoseLandmarkType.leftKnee],
-        [PoseLandmarkType.leftKnee, PoseLandmarkType.leftAnkle],
-        [PoseLandmarkType.rightHip, PoseLandmarkType.rightKnee],
-        [PoseLandmarkType.rightKnee, PoseLandmarkType.rightAnkle],
-      ];
-
-      for (final connection in connections) {
-        final startLandmark = landmarks[connection[0].index];
-        final endLandmark = landmarks[connection[1].index];
-        //調整完位置，符合角度。
-        double startX = startLandmark.x - 150;
-        double startY = startLandmark.y - 150;
-        double endX = endLandmark.x - 150;
-        double endY = endLandmark.y - 150;
-
-        // 如果是前置摄像头，进行垂直翻转
-        if (isFrontCamera) {
-          startX = size.width + 300 - startX;
-          endX = size.width + 300 - endX;
-          startY = size.width - 20 - startY;
-          endY = size.width - 20 - endY;
-        }
-
-        canvas.drawLine(
-          Offset(startX, startY),
-          Offset(endX, endY),
-          paint,
-        );
-      }
-    }
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
-  }
-}
 
 // 計算角度的函式
 double getAngle(
